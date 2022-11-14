4d75d22192b933b44ac523f09cc445d851800de1
seldish-og <jegorbasya@mail.ru>
2022-11-14 23:29:41 +0300

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Cryptov2
+# Cryptov2 Документация
+
+## Как запустить проект
+
+1. `git clone https://github.com/seldish-og/Cryptov2.git`
+2. Create and run Apache Cassandra (for example in Docker)
+3. Create venv: `python -m venv venv`, start venv: `source ./venv/bin/activate`
+4. Run `python ./app/database/init_db.py` to create tables in db
+5.
--- app/database/db/init_db.py
+++ app/database/db/init_db.py
@@ -6,12 +6,16 @@ cluster = Cluster()
 
 def create_keyspace():
     session = cluster.connect()
-    session.execute('''create keyspace pairskeyspace with replication={
+    session.execute('''
+    create keyspace pairskeyspace with replication={
     'class': 'SimpleStrategy', 'replication_factor' : 1
-    };''')
+    };
+    ''')
 
 
 def create_table():
     session = cluster.connect('pairskeyspace')
-    create_db_qry = '''CREATE TABLE testssrt (id int PRIMARY_KEY, name text);'''
+    create_db_qry = '''
+    CREATE TABLE testssrt (id int PRIMARY_KEY, name text);
+    '''
     session.execute(create_db_qry)
