ab1f1e6aaaf7bdd4edda980fac22264c7dfb185d
seldish-og <jegorbasya@mail.ru>
2022-11-14 23:35:34 +0300

    some updates

--- app/coingecko_parser/parser.py
+++ app/coingecko_parser/parser.py
@@ -3,4 +3,21 @@ from bs4 import BeautifulSoup
 
 
 class Parser:
-    pass
+    def __init__(self) -> None:
+        self.url = "https://www.coingecko.com/"
+        self.params = {}
+
+    def get_page_content(self):
+        page = requests.get(self.url)
+        return page.text
+
+    def parse_html(self):
+        page_text = self.get_page_content()
+        soup = BeautifulSoup(page_text, "html.parser")
+        results = soup.find_all("span", class_="lg:tw-flex")
+        for i in results:
+            print(i.text)
+
+
+prs = Parser()
+print(prs.parse_html())
