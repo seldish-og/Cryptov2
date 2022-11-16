81e6c5c1f8d1fd3635467440d1f244309300ff7c
seldish-og <jegorbasya@mail.ru>
2022-11-16 23:23:36 +0300

    finish init_db

--- app/database/init_db.py
+++ app/database/init_db.py
@@ -13,9 +13,39 @@ def create_keyspace():
     ''')
 
 
-def create_table():
+def create_coins_tables(session):
+    create_coins_db_qry = '''
+    CREATE TABLE coins (
+        id int,
+        symbol text,
+        coin_name text,
+        day_volume int,
+        markets list<int>, 
+        primary key(id)
+        );'''
+    session.execute(create_coins_db_qry)
+
+
+def create_markets_fees_tables(session):
+    markets = ["huobi", "bitget", "mexc", "bybit", "kucoin", "gateio"]
+
+    for market in markets:
+        create_market_fees_db_qry = f'''
+        CREATE TABLE {market}_fees (
+            id int,
+            symbol text,
+            coin_name text,
+            withdrawal_fee int,
+            buy_sell_fee int,
+            min_withdrawal int,
+            max_withdrawal int,
+            primary key(id)
+            );'''
+        session.execute(create_market_fees_db_qry)
+
+
+if __name__ == "__main__":
+    create_keyspace()
     session = cluster.connect('pairskeyspace')
-    create_db_qry = '''
-    CREATE TABLE testssrt (id int PRIMARY_KEY, name text);
-    '''
-    session.execute(create_db_qry)
+    create_coins_tables(session)
+    create_markets_fees_tables(session)
