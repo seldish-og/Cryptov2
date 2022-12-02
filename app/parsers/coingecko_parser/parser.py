807f35611a3d4f0e3ee3f1765b62c81214cd3a76
seldish-og <jegorbasya@mail.ru>
2022-12-02 18:45:57 +0300

    some updates

--- app/parsers/coingecko_parser/parser.py
+++ app/parsers/coingecko_parser/parser.py
@@ -13,7 +13,6 @@ class Parser:
 
     def parse_html(self):
         page_text = self.get_page_content()
-
         soup = BeautifulSoup(page_text, "html.parser")
         results = soup.find_all("span", class_="lg:tw-flex")
 
