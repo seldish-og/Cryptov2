284a7eb77ce56e73df876b66f0bb47b6c94b2183
seldish-og <marius@gaius>
2023-01-24 07:40:39 -0500

    some updates

--- app/parsers/coingecko_parser/parser.py
+++ app/parsers/coingecko_parser/parser.py
@@ -1,5 +1,11 @@
+import json
 import requests
 from bs4 import BeautifulSoup
+
+
+def write_json(data: dict):
+    with open('./files/json_data.json', 'w') as outfile:
+        json.dump(data, outfile)
 
 
 class Parser:
@@ -11,18 +17,32 @@ class Parser:
         page = requests.get(self.url)
         return page.text
 
-    def parse_html(self):
+    def get_html_data(self):
         page_text = self.get_page_content()
         soup = BeautifulSoup(page_text, "html.parser")
-        results = soup.find_all("span", class_="lg:tw-flex")
+        # crypto_names = soup.find_all("span", class_="lg:tw-flex")
+        day_volume = []
+        for i in soup.find_all("td", class_="td-liquidity_score"):
+            day_volume.append(
+                int(i.text.strip().replace("$", "").replace(",", "").replace(".", "")))
 
-        x = []
-        for i in results:
-            x.append(i.text)
-        print(x, len(x))
+        # market_cap = soup.find_all("td", class_="td-market_cap")
+        return day_volume, len(day_volume)
 
 
 prs = Parser()
+print(prs.get_html_data())
 
 
-print(prs.parse_html())
+# def prepare_parsed_data_to_save():
+#     crypto_names, day_volume, market_cap = prs.get_html_data()
+#     result_dict = {}
+#     for value in day_volume:
+#         if int(value.text) > 1000000:
+#             value_index = day_volume.index(value)
+#             result_dict[crypto_names[value_index]] = [
+#                 value, market_cap[value_index]]
+#     write_json(result_dict)
+
+
+# prepare_parsed_data_to_save()
