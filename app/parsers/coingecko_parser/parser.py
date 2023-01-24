694ba6e0e232ae58a65fc00f179f4734e6ab4101
seldish-og <marius@gaius>
2023-01-24 08:22:34 -0500

    some updates

--- app/parsers/coingecko_parser/parser.py
+++ app/parsers/coingecko_parser/parser.py
@@ -1,4 +1,6 @@
 import json
+import cchardet
+import lxml
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,13 +11,13 @@ def write_json(data: dict):
 
 
 class DataFormatter:
-    def format_strings(self, all_tags):
+    def format_string_tags(self, all_tags):
         strings = []
         for i in all_tags:
             strings.append(str(i.text.strip()))
         return strings
 
-    def format_digits(self, all_tags):
+    def format_int_tags(self, all_tags):
         digits = []
         for i in all_tags:
             digits.append(
@@ -34,36 +36,37 @@ class Parser(DataFormatter):
 
     def get_html_data(self):
         page_text = self.get_page_content()
-        soup = BeautifulSoup(page_text, "html.parser")
+        soup = BeautifulSoup(page_text, "lxml")
 
         all_crypto_names = soup.find_all("span", class_="lg:tw-flex")
-        crypto_names = self.format_strings(all_tags=all_crypto_names)
+        crypto_names = self.format_string_tags(all_tags=all_crypto_names)
 
-        all_crypto_symbols = soup.find_all("span", class_="lg:tw-flex")
-        crypto_symbols = self.format_strings(all_tags=all_crypto_names)
+        all_crypto_symbols = soup.find_all("span", class_="d-lg-inline")
+        crypto_symbols = self.format_string_tags(all_tags=all_crypto_symbols)
 
         all_day_volume = soup.find_all("td", class_="td-liquidity_score")
-        day_volume = self.format_digits(all_tags=all_day_volume)
+        day_volume = self.format_int_tags(all_tags=all_day_volume)
 
         all_market_cap = soup.find_all("td", class_="td-market_cap")
-        market_cap = self.format_digits(all_tags=all_market_cap)
+        market_cap = self.format_int_tags(all_tags=all_market_cap)
 
-        return crypto_names, len(crypto_names)
-
-
-prs = Parser()
-print(prs.get_html_data())
+        return crypto_names, crypto_symbols, day_volume, market_cap
 
 
-def prepare_parsed_data_to_save():
-    crypto_names, day_volume, market_cap = prs.get_html_data()
+parser = Parser()
+# print(prs.get_html_data())
+
+
+def check_and_save_data():
+    crypto_names, crypto_symbols, day_volume, market_cap = parser.get_html_data()
+
     result_dict = {}
     for value in day_volume:
-        if int(value.text) > 1000000:
+        if value > 1000000:
             value_index = day_volume.index(value)
             result_dict[crypto_names[value_index]] = [
-                value, market_cap[value_index]]
+                crypto_symbols[value_index], value, market_cap[value_index]]
     write_json(result_dict)
 
 
-# prepare_parsed_data_to_save()
+check_and_save_data()
