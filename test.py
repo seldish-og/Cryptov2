98801ed9b4a7c6e507a595ffebd727b98db0fae5
seldish-og <marius@gaius>
2023-02-15 15:40:13 -0500

    done parser and saving to json

---
+++ test.py
@@ -0,0 +1,16 @@
+# ищем 3 делителя кроме 1, 0, и самого number
+def task(number: int):
+    count = 0
+    for i in range(2, number // 2):
+        if number % i == 0:
+            print(i)
+            count += 1
+            if count >= 3:
+                return True
+    if count < 3:
+        return False
+
+
+# print(task(5))/
+# print(task(10))
+print(task(15002300))
